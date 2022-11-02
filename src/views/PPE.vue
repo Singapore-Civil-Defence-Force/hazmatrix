0a75b9a25fac34ebfa059aeaea75ec9694d69260
Jaime <jaimeloeuf@gmail.com>
2022-11-02 12:30:20 +0800

    Update PPE view to include pictures.

--- src/views/PPE.vue
+++ src/views/PPE.vue
@@ -4,23 +4,41 @@ import PPEs from "../../data/PPE.json";
 // Get PPE's id from router
 const { id } = defineProps<{ id: string }>();
 
+// Ignoring the TS warning because we can be sure that the id being passed in as props
+// is valid since the parent component is taking the value from the PPE.json directly.
+// @ts-ignore
 const PPE = PPEs[id];
+
+// Need to do this to get the image link dynamically and cannot be done inside the template
+// Reference: https://stackoverflow.com/questions/66419471/vue-3-vite-dynamic-img-src
+const image = new URL(`../assets/PPE/${id}.jpg`, import.meta.url).href;
 </script>
 
 <template>
-  <!-- @todo Show a picture of the PPE -->
-  <div class="center">
-    <h1 class="title px-6">
-      {{ PPE.full }}
-    </h1>
+  <div class="columns is-mobile is-multiline has-text-left m-2">
+    <div class="column is-narrow">
+      <router-link
+        class="button is-light is-fullwidth is-danger"
+        :to="{ name: 'home' }"
+      >
+        Home
+      </router-link>
+    </div>
+
+    <div class="column">
+      <button class="button is-light is-fullwidth" @click="$router.back()">
+        Back
+      </button>
+    </div>
+
+    <div class="column is-full">
+      <h1 class="title has-text-left">
+        {{ PPE.full }}
+      </h1>
+    </div>
+
+    <div class="column is-full">
+      <img class="image" :src="image" alt="PPE" />
+    </div>
   </div>
 </template>
-
-<style scoped>
-.center {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-}
-</style>
