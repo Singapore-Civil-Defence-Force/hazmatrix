98ef523662336ee30c7a49f0ac94cc15b9a55fac
Jaime <jaimeloeuf@gmail.com>
2022-11-03 22:01:15 +0800

    Make Home view vertically aligned:     So that it looks more natural to users after installing the app to     their home screens, by wrapping the whole home view in a vertically     centered div.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,65 +8,79 @@ const scanQR = ref<boolean>(false);
 </script>
 
 <template>
-  <div class="columns is-multiline mx-1">
-    <!-- Only load/show QR Code Scanner modal when requested -->
-    <div v-if="scanQR" class="column is-full">
-      <!-- Close QR Code scanner once code has been detected or if user closed the camera -->
-      <QRCodeScanner
-        v-on:close-camera="scanQR = false"
-        v-on:qrcode-detected="scanQR = false"
-      />
-    </div>
+  <div class="center">
+    <div class="columns is-multiline">
+      <!-- Only load/show QR Code Scanner modal when requested -->
+      <div v-if="scanQR" class="column is-full">
+        <!-- Close QR Code scanner once code has been detected or if user closed the camera -->
+        <QRCodeScanner
+          v-on:close-camera="scanQR = false"
+          v-on:qrcode-detected="scanQR = false"
+        />
+      </div>
 
-    <div class="column is-full">
-      <button
-        v-if="scanQR"
-        class="button is-light is-fullwidth is-warning py-6 is-size-4"
-        @click="scanQR = false"
-      >
-        Close QR Scanner
-      </button>
+      <div class="column is-full">
+        <button
+          v-if="scanQR"
+          class="button is-light is-fullwidth is-warning py-6 is-size-4"
+          @click="scanQR = false"
+        >
+          Close QR Scanner
+        </button>
 
-      <button
-        v-else
-        class="button is-light is-fullwidth is-warning py-6 is-size-4"
-        @click="scanQR = true"
-      >
-        Scan HazMatrix QR code
-      </button>
-    </div>
+        <button
+          v-else
+          class="button is-light is-fullwidth is-warning py-6 is-size-4"
+          @click="scanQR = true"
+        >
+          Scan HazMatrix QR code
+        </button>
+      </div>
 
-    <div class="column is-full">
-      <router-link
-        :to="{ name: 'search' }"
-        class="button is-light is-fullwidth is-success py-6 is-size-4"
-      >
-        Chemical
-      </router-link>
-    </div>
+      <div class="column is-full">
+        <router-link
+          :to="{ name: 'search' }"
+          class="button is-light is-fullwidth is-success py-6 is-size-4"
+        >
+          Chemical
+        </router-link>
+      </div>
 
-    <div class="column is-full">
-      <router-link
-        :to="{ name: 'search-equipment' }"
-        class="button is-light is-fullwidth is-warning py-6 is-size-4"
-      >
-        Equipment
-      </router-link>
-    </div>
+      <div class="column is-full">
+        <router-link
+          :to="{ name: 'search-equipment' }"
+          class="button is-light is-fullwidth is-warning py-6 is-size-4"
+        >
+          Equipment
+        </router-link>
+      </div>
 
-    <div class="column is-full">
-      <router-link
-        :to="{ name: 'foam-calculator' }"
-        class="button is-light is-fullwidth is-success py-6 is-size-4"
-      >
-        Foam Calculator
-      </router-link>
-    </div>
+      <div class="column is-full">
+        <router-link
+          :to="{ name: 'foam-calculator' }"
+          class="button is-light is-fullwidth is-success py-6 is-size-4"
+        >
+          Foam Calculator
+        </router-link>
+      </div>
 
-    <div class="column is-full">
-      <router-link :to="{ name: 'about' }" class="button is-light is-fullwidth">
-        About / Help
-      </router-link>
+      <div class="column is-full">
+        <router-link
+          :to="{ name: 'about' }"
+          class="button is-light is-fullwidth"
+        >
+          About / Help
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
